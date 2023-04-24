47b11e04b21895139900ff46e54f40e7cb4fdc6d
srujanKs5 <127977584+srujanKs5@users.noreply.github.com>
2023-04-24 22:59:27 +0530

    Delete ECS_Fargate-Cluster-master directory

--- ECS_Fargate-Cluster-master/ecs.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ecs_cluster" "ecs-cluster" {
-  name = format("%s-%s-ecs-cluster", var.dns_name, var.account_environment)
-  tags = {
-    Name = format("%s-%s-ecs-cluster", var.dns_name, var.account_environment) 
-  }
-}
--- ECS_Fargate-Cluster-master/environment/dev.backend.tfvars
+++
@@ -1,4 +0,0 @@
-# Terraform state
-bucket = "kartheeks-bucket"
-key = "terraform_state/infrastructure/dev/terraform.tfstate"
-region = "us-east-1"
