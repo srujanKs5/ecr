5e9fa0c2d08d6f82ad9f7c3ce257088b8f757646
srujanKs5 <127977584+srujanKs5@users.noreply.github.com>
2023-04-26 22:45:33 +0530

    Delete ECS_Fargate-Cluster-master directory

--- ECS_Fargate-Cluster-master/ecr.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ecr_repository" "service-1" {
-  name = format("%s-%s-int-service-1-ecr",var.dns_name, var.account_environment)
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
--- ECS_Fargate-Cluster-master/environment/dev.backend.tfvars
+++
@@ -1,4 +0,0 @@
-# Terraform state
-bucket = "srujankeys"
-key = "terraform_state/infrastructure/dev/terraform.tfstate"
-region = "us-east-1"
